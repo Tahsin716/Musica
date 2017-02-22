bae3e8ab67acaf1d6350d024a9b534b601123c58
Tahsin Rashad <tahsin.rashad716@gmail.com>
2017-02-23 02:17:59 +0600

    Player update - 1

--- app/src/main/java/com/randomapps/musica/Player.java
+++ app/src/main/java/com/randomapps/musica/Player.java
@@ -2,10 +2,31 @@ package com.randomapps.musica;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.io.File;
+import java.util.ArrayList;
 
 
 public class Player extends AppCompatActivity {
+
+    //song position in ListView
+    int position;
+    SongManager songManager;
+
+    //Playlist
+    ArrayList<File> songList;
+    //Song name
+    String path;
+
+    ImageView albumArt;
+    TextView name, artist;
+    ImageButton play, prev, next, playlist;
+    SeekBar seekBar;
+    String artistName, songName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
