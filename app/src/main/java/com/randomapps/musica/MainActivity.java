8402674dc5a7fac1ad560e0066e606ac40b6c582
Tahsin Rashad <tahsin.rashad716@gmail.com>
2017-02-22 19:49:13 +0600

    SongManager class update - 1

--- app/src/main/java/com/randomapps/musica/MainActivity.java
+++ app/src/main/java/com/randomapps/musica/MainActivity.java
@@ -2,7 +2,6 @@ package com.randomapps.musica;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
