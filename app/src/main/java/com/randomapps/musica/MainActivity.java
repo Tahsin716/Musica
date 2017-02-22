34c1441041f5834c6b0aae14566419beb0ded8b1
Tahsin Rashad <tahsin.rashad716@gmail.com>
2017-02-23 00:09:32 +0600

    Method init implemented and also overriden onBackPressed

--- app/src/main/java/com/randomapps/musica/MainActivity.java
+++ app/src/main/java/com/randomapps/musica/MainActivity.java
@@ -1,44 +1,65 @@
 package com.randomapps.musica;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.ListView;
+
 
 import java.io.File;
+import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    ArrayList<File> songList;
+    SongManager songManager;
+    ProgressDialog progressDialog;
+    File root;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //This gives directory of the external memory (SD card)
-        File root = Environment.getExternalStorageDirectory();
+        init();
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    /**
+     * Used to initialize all the components
+     * set the dialog and fetch the songs
+     */
+    public void init() {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        listView = (ListView)findViewById(R.id.listView);
+        songManager = new SongManager();
+        //Path to external storage directory, in this case SD card
+        root = Environment.getExternalStorageDirectory();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Please wait, while songs are loading...");
+        //The dialog is cancelable with BACK key
+        progressDialog.setCancelable(true);
+
+        //Display the progress dialog, until all songs have been fetched
+        while(!songManager.getFetchStatus()) {
+            songList = songManager.findSongList(root);
+        }
+        if(songList != null){
+            progressDialog.dismiss();
         }
 
-        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Close the app if user presses BACK key
+     */
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        this.finish();
     }
 }
