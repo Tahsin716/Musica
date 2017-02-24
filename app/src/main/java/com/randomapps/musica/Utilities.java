3583ffa0c8e34a024857cd74a34dc155a9586f9e
Tahsin Rashad <tahsin.rashad716@gmail.com>
2017-02-24 20:55:39 +0600

    milliSecondsToTimer

--- app/src/main/java/com/randomapps/musica/Utilities.java
+++ app/src/main/java/com/randomapps/musica/Utilities.java
@@ -3,4 +3,33 @@ package com.randomapps.musica;
 
 public class Utilities {
 
+    /**
+     * Function to convert milliseconds time to
+     * Timer Format
+     * Hours:Minutes:Seconds
+     */
+    public String milliSecondsToTimer(long milliseconds){
+        String finalTimerString = "";
+        String secondsString;
+
+        // Convert total duration into time
+        int hours = (int)( milliseconds / (3600));
+        int minutes = (int)(milliseconds % (3600)) / (1000*60);
+        int seconds = (int) ((milliseconds % (3600)) % (1000*60) / 1000);
+        // Add hours if there
+        if(hours > 0){
+            finalTimerString = hours + ":";
+        }
+
+        // Prepending 0 to seconds if it is one digit
+        if(seconds < 10){
+            secondsString = "0" + seconds;
+        }else{
+            secondsString = "" + seconds;}
+
+        finalTimerString = finalTimerString + minutes + ":" + secondsString;
+
+        // return timer string
+        return finalTimerString;
+    }
 }
