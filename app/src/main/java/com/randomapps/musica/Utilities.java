b4f33738d28a183391f8697cd1245d932949291d
Tahsin Rashad <tahsin.rashad716@gmail.com>
2017-02-24 20:56:43 +0600

    progressPercentage

--- app/src/main/java/com/randomapps/musica/Utilities.java
+++ app/src/main/java/com/randomapps/musica/Utilities.java
@@ -32,4 +32,21 @@ public class Utilities {
         // return timer string
         return finalTimerString;
     }
+
+
+    /**
+     * Function to get Progress percentage
+     */
+    public int getProgressPercentage(long currentDuration, long totalDuration){
+        Double percentage = (double) 0;
+
+        long currentSeconds = (int) (currentDuration / 1000);
+        long totalSeconds = (int) (totalDuration / 1000);
+
+        // calculating percentage
+        percentage =(((double)currentSeconds)/totalSeconds)*100;
+
+        // return percentage
+        return percentage.intValue();
+    }
 }
